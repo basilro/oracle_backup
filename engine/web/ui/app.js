56eef5b82742a0bc40fccd7f527af5e6bdd881ea
bcjang <sica4311@gmail.com>
2026-06-08 12:06:04 +0900

    chore(ui): 캐시버스트 스탬프 ui-2026-06-08a

--- engine/web/ui/app.js
+++ engine/web/ui/app.js
@@ -1,4 +1,4 @@
-const BUILD = "ui-2026-06-03b";
+const BUILD = "ui-2026-06-08a";
 let csrf = "";
 const $ = s => document.querySelector(s);
 const esc = s => String(s ?? "").replace(/[&<>"']/g, c => ({ "&": "&amp;", "<": "&lt;", ">": "&gt;", '"': "&quot;", "'": "&#39;" }[c]));
